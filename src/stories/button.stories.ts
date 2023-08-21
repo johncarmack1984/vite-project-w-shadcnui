22b52fd8a8ed77b22ab0d29544205b7879fbd0d3
John Carmack <johncarmack@me.com>
2023-08-21 10:32:06 -0700

    Add Storybook config, dependencies and example story

---
+++ src/stories/button.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "@/components/ui/button";
+
+const meta = {
+  title: "Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      options: [
+        "default",
+        "destructive",
+        "outline",
+        "secondary",
+        "ghost",
+        "link",
+      ],
+    },
+    size: { options: ["default", "sm", "lg", "icon"] },
+    asChild: { control: "boolean" },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "Button",
+  },
+};
