774381460eaf2981cef1628ebeafe9b045a745e0
John Carmack <johncarmack@me.com>
2023-08-21 11:31:01 -0700

    Update button stories with new story cases

--- src/stories/button.stories.ts
+++ src/stories/button.stories.ts
@@ -19,13 +19,13 @@ const meta = {
         "ghost",
         "link",
       ],
-      control: { type: "radio" },
+      control: { type: "inline-radio" },
       defaultValue: "default",
       description:
         "Changes the look and feel of the button to match the context of the page.",
       type: { name: "string", required: false },
     },
-    size: { options: ["default", "sm", "lg", "icon"] },
+    size: { options: ["default", "sm", "lg", "icon"], defaultValue: "default" },
     asChild: { type: "boolean", defaultValue: false },
     children: { type: "string", defaultValue: "Button" },
   },
@@ -38,5 +38,30 @@ export const Default: Story = {
   args: {
     variant: "default",
     size: "default",
+    children: meta.argTypes.children.defaultValue,
+  },
+};
+
+export const Small: Story = {
+  args: {
+    variant: "default",
+    size: "sm",
+    children: meta.argTypes.children.defaultValue,
+  },
+};
+
+export const Large: Story = {
+  args: {
+    variant: "default",
+    size: "lg",
+    children: meta.argTypes.children.defaultValue,
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    variant: "default",
+    size: "icon",
+    children: "👍",
   },
 };
