7fcfd0db879419c434a3f745f42a2472f031d82c
John Carmack <johncarmack@me.com>
2023-08-21 19:01:31 -0700

    Add dark mode color support in stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,4 @@
 import type { Preview } from "@storybook/react";
-
-import React from "react";
-import { withThemeByClassName } from "@storybook/addon-styling";
-
-/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
 import "../src/index.css";
 
 const preview: Preview = {
@@ -16,18 +11,6 @@ const preview: Preview = {
       },
     },
   },
-
-  decorators: [
-    // Adds theme switching support.
-    // NOTE: requires setting "darkMode" to "class" in your tailwind config
-    withThemeByClassName({
-      themes: {
-        light: "light",
-        dark: "dark",
-      },
-      defaultTheme: "light",
-    }),
-  ],
 };
 
 export default preview;
