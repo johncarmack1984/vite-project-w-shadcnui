0d844735d69b64adb8cd7808c892827650d5ee92
John Carmack <johncarmack@me.com>
2023-08-22 13:17:27 -0700

    Add Select component and update package.json

---
+++ src/stories/select.stories.tsx
@@ -0,0 +1,67 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const meta = {
+  title: "Select",
+  component: Select,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Select>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const SelectRender: Story = {
+  render: ({ children, ...args }) => {
+    return (
+      <div className="flex flex-row">
+        <div className="p-5 border-2 rounded-l-md">
+          <Select {...args}>
+            <SelectTrigger>
+              <SelectValue>Option 1</SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Label</SelectLabel>
+                <SelectItem value="option 1">Option 1</SelectItem>
+                <SelectItem value="option 2">Option 2</SelectItem>
+                <SelectItem value="option 3">Option 3</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="dark p-5 bg-background rounded-r-md">
+          <Select {...args}>
+            <SelectTrigger className="dark">
+              <SelectValue>Option 1</SelectValue>
+            </SelectTrigger>
+            <SelectContent className="dark">
+              <SelectGroup>
+                <SelectLabel>Label</SelectLabel>
+                <SelectItem value="option 1">Option 1</SelectItem>
+                <SelectItem value="option 2">Option 2</SelectItem>
+                <SelectItem value="option 3">Option 3</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    );
+  },
+};
+
+export const Default: Story = {
+  ...SelectRender,
+};
